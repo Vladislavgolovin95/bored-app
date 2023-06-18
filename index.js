4f362787be078a4f89907412df29b2a2a57d2414
 home <home@Mac-mini-home.local>
2023-06-18 21:51:40 +0300

    wrote markup, styles and js

---
+++ index.js
@@ -0,0 +1,24 @@
+// Объявляем переменные 
+const boredWrapper = document.getElementById('bored');
+const titleNode = document.getElementById('bored__title');
+const textNode = document.getElementById('bored__text');
+const btnActiveNode = document.getElementById('bored__btn');
+
+// Обработчик событий при клике на кнопку
+btnActiveNode.addEventListener('click', function() {
+    fetch('http://www.boredapi.com/api/activity/')
+    .then(activity => activity.json())
+    .then((res) => {
+
+        const textBored = res.activity;
+
+        textNode.innerHTML = `<p>${textBored}</p>`;
+    });
+    renderWrapper();
+});
+
+// Обновляем и отрисовываем блок
+function renderWrapper() {
+    boredWrapper.classList.add('bored-active');
+    titleNode.innerText = 'Have fun!🔥';
+};
